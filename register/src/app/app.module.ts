7e2ef01be93dc4958760d8a8b1d180f581777e77
Oby <obyone12@gmail.com>
2021-06-24 19:22:10 +0700

    ok

---
+++ register/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Route } from '@angular/router';
+import { AppComponent } from './app.component';
+
+import { FormCreate } from './posts/Register/form.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { FormGroup, FormsModule,ReactiveFormsModule } from '@angular/forms';
+
+
+
+import { TicketComponent } from './posts/Search/ticket.component';
+
+
+const appRoute : Route[]= [
+  {path:'Register', component: FormCreate},
+  {path:'Search', component: TicketComponent},
+  {path: '',redirectTo:'Register', pathMatch:'full'}
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormCreate,
+    TicketComponent
+
+  ],
+  imports: [
+    RouterModule.forRoot(appRoute),
+    BrowserModule,
+    HttpClientModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
